a4ac7e27c880936cbe8c88387aa1c6603bbb4f23
Lee Clayberg <lee.clayberg@gmail.com>
2020-05-26 14:42:07 -0400

    Added in react project

---
+++ src/main/webapp/precache-manifest.6b11cd093f5e7cf1146416c31f3c8aeb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed021e6f4016120e8c70155b47959bd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1517480401187334d41c",
+    "url": "/static/css/2.a17a1ed7.chunk.css"
+  },
+  {
+    "revision": "3c14953729076a9a04ea",
+    "url": "/static/css/main.dcf367d8.chunk.css"
+  },
+  {
+    "revision": "1517480401187334d41c",
+    "url": "/static/js/2.4f9e056d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4f9e056d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c14953729076a9a04ea",
+    "url": "/static/js/main.d9bf482b.chunk.js"
+  },
+  {
+    "revision": "ad18916de1de9e7143ac",
+    "url": "/static/js/runtime-main.8be48931.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b43f4e05db74cb04bc236d98449b100f",
+    "url": "/static/media/preview-image.b43f4e05.png"
+  }
+]);
