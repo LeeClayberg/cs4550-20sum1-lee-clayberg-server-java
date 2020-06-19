dc98999076377c84aad1387d2a6a9eeac2f56298
Lee Clayberg <lee.clayberg@gmail.com>
2020-06-19 16:03:04 -0400

    Topic repository from starter code

---
+++ src/main/java/com/example/cs455020su1leeclaybergserverjava/repositories/TopicRepository.java
@@ -0,0 +1,23 @@
+package com.example.cs455020su1leeclaybergserverjava.repositories;
+
+import com.example.cs455020su1leeclaybergserverjava.models.Topic;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TopicRepository extends CrudRepository<Topic, Integer> {
+
+  @Query("SELECT topic FROM Topic topic")
+  public List<Topic> findAllTopics();
+
+  @Query("SELECT topic FROM Topic topic WHERE topic.id=:topicId")
+  public Topic findTopicById(
+          @Param("topicId") Integer tid);
+
+  @Query("SELECT topic FROM Topic topic WHERE topic.lesson = :lessonId")
+  List<Topic> findTopicsForLesson(
+          @Param("lessonId") String lessonId);
+}
